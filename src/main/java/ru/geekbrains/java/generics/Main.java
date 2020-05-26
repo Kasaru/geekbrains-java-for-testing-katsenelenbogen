e831796c1b4c2e527e599f720bbea62db4ec4690
Kasaru <artemiy064@bk.ru>
2020-05-27 00:28:33 +0400

    Создать таблицу с пользователями (users). У пользователя есть следующие поля: id, имя, возраст, email; Написать метод, распечатывающий в консоль всю информацию обо всех пользователях из таблицы; Написать метод вывода в консоль пользователей, возраст которых находится в пределах min-max (public void showUsersByAge(int min, int max)); Написать метод удаления пользователя по имени; Написать метод добавления пользователя в таблицу users из строки вида “имя возраст email”. Например, “Bob 32 bob32@gmail.com”.

--- src/main/java/ru/geekbrains/java/generics/Main.java
+++ src/main/java/ru/geekbrains/java/generics/Main.java
@@ -1,5 +1,31 @@
 package ru.geekbrains.java.generics;
 
-public class Main {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        DB.clear();
+        DB.createTable();
+
+        int count = 200;
+        List<String> users = DB.createUsers(count);
+        DB.addToTable(users);
+        System.out.printf("В базу данных добавлено %d новых пользователей\n", count);
+
+        System.out.println("Таблица users:");
+        DB.getFullTable();
+
+        int min = 10;
+        int max = 50;
+        System.out.printf("Пользователи старше %d и младше %d\n", min, max);
+        DB.getUsersByAge(min,max);
+
+
+
+    }
 }
